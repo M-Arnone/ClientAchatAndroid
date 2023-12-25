1fb9be0e95ea4023943cec85508326dc5f056498
m-arnone <matteo.arnone@student.hepl.be>
2023-12-25 01:02:17 +0100

    listarticles - show images

--- app/src/main/java/com/example/clientachatandroid/network/NetworkManager.java
+++ app/src/main/java/com/example/clientachatandroid/network/NetworkManager.java
@@ -2,6 +2,7 @@ package com.example.clientachatandroid.network;
 
 import android.content.Context;
 import android.os.AsyncTask;
+import com.example.clientachatandroid.model.Article;
 import com.example.clientachatandroid.model.Model;
 
 import java.io.IOException;
@@ -10,10 +11,12 @@ import java.sql.SQLException;
 public class NetworkManager {
 
     private Context applicationContext;
+    private Model m;
 
 
-    public NetworkManager(Context applicationContext) {
+    public NetworkManager(Context applicationContext) throws SQLException, IOException, ClassNotFoundException {
         this.applicationContext = applicationContext;
+        m = Model.getInstance(applicationContext);
     }
 
     public void performLoginAsync(String username, String password, OnLoginCompleteListener listener) {
@@ -30,12 +33,11 @@ public class NetworkManager {
         @Override
         protected Void doInBackground(String... params) {
             try {
-                Model m = Model.getInstance(applicationContext);
                 m.on_pushLogin(params[0], params[1], false);
-                //Boolean.parseBoolean(params[2])
-            } catch (SQLException | IOException | ClassNotFoundException e) {
+            } catch (IOException e) {
                 throw new RuntimeException(e);
             }
+            //Boolean.parseBoolean(params[2])
 
             // À la fin, appeler la méthode de l'interface lorsque l'opération est terminée
             if (listener != null) {
@@ -46,8 +48,49 @@ public class NetworkManager {
         }
     }
 
+    public void fetchArticleAsync(int articleId, OnArticleFetchListener listener) {
+        new FetchArticleTask(listener).execute(articleId);
+    }
+
+    private class FetchArticleTask extends AsyncTask<Integer, Void, Article> {
+        private final OnArticleFetchListener listener;
+
+        public FetchArticleTask(OnArticleFetchListener listener) {
+            this.listener = listener;
+        }
+
+        @Override
+        protected Article doInBackground(Integer... params) {
+            try {
+                // Effectuez vos opérations réseau ici (ex. récupérer l'article du serveur)
+                return m.setArticle(params[0]);
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(Article article) {
+            super.onPostExecute(article);
+            if (article != null) {
+                // Appeler la méthode de l'interface lorsque l'opération est terminée avec succès
+                listener.onArticleFetched(article);
+            } else {
+                // Appeler la méthode de l'interface en cas d'erreur
+                listener.onArticleFetchError("Erreur lors de la récupération de l'article");
+            }
+        }
+    }
+
+
+
     // Interface pour écouter la fin du login
     public interface OnLoginCompleteListener {
         void onLoginComplete();
+    }
+    public interface OnArticleFetchListener {
+        void onArticleFetched(Article article);
+        void onArticleFetchError(String errorMessage);
     }
 }
